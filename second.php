66ff0426489fe74c67b1edec26cb04c2a45423d0
briannnn909 <leonardobrianmckaine@gmail.com>
2025-07-11 09:34:27 +0800

    Add files via upload

---
+++ second.php
@@ -0,0 +1,17 @@
+<?php
+echo "Prince nasuspend sa mcdo <br>";
+echo "Prince walang kwenta <br><pre>";
+
+
+
+  
+
+echo " ('.   ,.---.,   .')
+  \ `-'       `-' / 
+ __\;   O   O   `/__
+(__/     ___     \__)
+   (    /, ,\    )
+    \   \___/   /    
+ dwb `~~~~~~~~~'";
+
+?>
